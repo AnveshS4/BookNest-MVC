55a8859897472581f9f037e6f8585fa12b7a139a
AnveshS4 <santhosam.anvesh@gmail.com>
2024-12-03 09:34:28 -0500

    First commit: MVC-based BookStore with advanced featuresAdd project files.

--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -34,17 +34,17 @@ namespace Bulky.Models
         public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
-        [Required]
+       
         public string PhoneNumber { get; set; }
-        [Required]
+      
         public string StreetAddress { get; set; }
-        [Required]
+     
         public string City { get; set; }
-        [Required]
+        
         public string State { get; set; }
-        [Required]
+        
         public string PostalCode { get; set; }
-        [Required]
+       
         public string Name { get; set; }
 
 
